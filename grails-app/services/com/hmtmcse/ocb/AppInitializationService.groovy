b6553397e844c6c1e4d26c65005054cc1ed32f53
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-07-31 12:03:50 +0530

    first commit

---
+++ grails-app/services/com/hmtmcse/ocb/AppInitializationService.groovy
@@ -0,0 +1,21 @@
+package com.hmtmcse.ocb
+
+
+class AppInitializationService {
+
+    static initialize() {
+        initMember()
+    }
+
+    private static initMember() {
+        if (Member.count() == 0) {
+            Member member = new Member()
+            member.firstName = "System"
+            member.lastName = "Administrator"
+            member.email = "mayur@admin.com"
+            member.password = "654321"
+            member.memberType = GlobalConfig.USER_TYPE.ADMINISTRATOR
+            member.save(flash: true)
+        }
+    }
+}
